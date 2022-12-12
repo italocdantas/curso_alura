f1f55d02859b1aafe5390b6fd8c4617f0f39269d
italocdantas <italocampdantas@gmail.com>
2022-12-12 17:55:33 -0300

    adição do arquivo banco.php

---
+++ avancando/banco.php
@@ -0,0 +1,44 @@
+<?php
+
+function exibeMensagem($mensagem){
+    echo $mensagem . PHP_EOL;
+};
+
+function depositar($conta,$valorADepositar){
+    $conta['saldo'] += $valorADepositar;
+    return $conta;
+};
+
+function sacar($conta, $valorASacar)
+{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem($conta['titular']. " " . "Você não pode sacar este valor");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+    return $conta;
+}
+
+$contasCorrentes = [
+    '123.456.789-10' => [
+        'titular' => 'Maria',
+        'saldo' => 10000
+    ],
+    '123.456.789-11' => [
+        'titular' => 'Alberto',
+        'saldo' => 300
+    ],
+    '123.256.789-12' => [
+        'titular' => 'Vinicius',
+        'saldo' => 100
+    ]
+];
+
+
+$contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'], 500);
+$contasCorrentes['123.456.789-11'] = sacar($contasCorrentes['123.456.789-11'], 500);
+$contasCorrentes['123.456.789-12'] = depositar($contasCorrentes['123.456.789-12'], 500);
+
+foreach ($contasCorrentes as $cpf => $conta) {
+    exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
+}
--- avancando/listas.php
+++ avancando/listas.php
@@ -3,3 +3,4 @@ $idadeList = [21, 23, 25, 32, 41];
 for ($i = 0; $i < count($idadeList); $i++){
     echo $idadeList[$i] . PHP_EOL;
 }
+
