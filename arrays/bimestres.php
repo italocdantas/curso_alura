aac9c15d89a5601e119a7f9b703481567aa93297
Italo <italocampdantas@gmail.com>
2023-01-03 13:17:32 -0300

    bimestres

--- arrays/bimestres.php
+++ arrays/bimestres.php
@@ -1,8 +1,20 @@
 <?php
-$notas = [
+$notasBimestre1 = [
     'Ana' => 10,
     'João' => 8,
     'Maria' => 9,
     'Roberto' => 7,
     'Vinicius' => 6
 ];
+
+
+
+$notasBimestre2 = [
+    'Ana' => 8,
+    'João' => 6,
+    'Maria' => 4,
+    'Roberto' => 5,
+];
+
+
+var_dump(array_diff_key($notasBimestre1, $notasBimestre2));
--- arrays/notas.php
+++ arrays/notas.php
@@ -5,22 +5,23 @@ $notas = [
     'Maria' => 9,
     'Roberto' => 7,
     'Vinicius' => 6
-    ];
+];
 
-    ksort($notas);
-    var_dump($notas);
+ksort($notas);
+var_dump($notas);
 
-    if(is_array($notas)){
-        echo "Sim, é um array!!!";
-    };
+if (is_array($notas)) {
+    echo "Sim, é um array!!!";
+};
 
-   var_dump( array_is_list($notas));
-   
-   echo 'Ana fez a prova: ' . PHP_EOL;
-   var_dump(isset($notas['Ana']));
+var_dump(array_is_list($notas));
 
-   echo 'Alguem tirou nota 10? ' . PHP_EOL;
-   var_dump(in_array(10,$notas,true));
+echo 'Ana fez a prova: ' . PHP_EOL;
+var_dump(isset($notas['Ana']));
 
-   echo 'Quem tirou 10? ' . PHP_EOL;
-   echo array_search(10, $notas) . PHP_EOL;
+echo 'Alguem tirou nota 10? ' . PHP_EOL;
+var_dump(in_array(10, $notas, true));
+
+echo 'Quem tirou 10? ' . PHP_EOL;
+echo array_search(10, $notas) . PHP_EOL;
+
