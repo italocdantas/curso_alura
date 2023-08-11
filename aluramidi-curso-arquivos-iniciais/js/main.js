96cb5c7526c067ab001ddcfa94b8b119a309e7a8
Italo <italocampdantas@gmail.com>
2023-08-11 00:13:14 -0300

    Finalizando os sons

--- aluramidi-curso-arquivos-iniciais/js/main.js
+++ aluramidi-curso-arquivos-iniciais/js/main.js
@@ -1,7 +1,25 @@
-function tocaSomPom (){
-    document.querySelector('#som_tecla_pom').play()
+function tocaSomPom (idElementoAudio){
+    document.querySelector(idElementoAudio).play()
 }
 
 
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idTecla = `#som_${instrumento}`;
+
+    tecla.onclick = function (){
+        tocaSomPom(idTecla);
+    };
+    contador++;
+    
+}
+
