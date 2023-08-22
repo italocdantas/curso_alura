c61316ac29a2202aa719ea702353583101abfd18
Italo <italocampdantas@gmail.com>
2023-08-21 23:39:11 -0300

    Adicionando comentarios

--- aluramidi-curso-arquivos-iniciais/js/main.js
+++ aluramidi-curso-arquivos-iniciais/js/main.js
@@ -12,8 +12,13 @@ let contador = 0;
 
 while (contador < listaDeTeclas.length){
 
+    //recebe  a listagem
     const tecla = listaDeTeclas[contador];
+    
+    //acessa a classe
     const instrumento = tecla.classList[1];
+
+    //concatena a classe do botão com a classe do som
     const idTecla = `#som_${instrumento}`;
 
     tecla.onclick = function (){
